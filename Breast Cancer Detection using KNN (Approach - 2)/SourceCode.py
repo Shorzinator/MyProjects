968f8ee98f6bfb6537648969f79ae9e2feaec3bb
Shorzinator <62899599+Shorzinator@users.noreply.github.com>
2021-03-10 13:18:14 +0530

    Loading dataset

--- Breast Cancer Detection using KNN (Approach - 2)/SourceCode.py
+++ Breast Cancer Detection using KNN (Approach - 2)/SourceCode.py
@@ -23,3 +23,10 @@ from sklearn.metrics import accuracy_score
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Loading Dataset from UCI Database
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"
+names = ['id', 'clump_thickness', 'uniform_cell_size', 'uniform_cell_shape',
+       'marginal_adhesion', 'single_epithelial_size', 'bare_nuclei',
+       'bland_chromatin', 'normal_nucleoli', 'mitoses', 'class']
+df = pd.read_csv(url, names=names)
