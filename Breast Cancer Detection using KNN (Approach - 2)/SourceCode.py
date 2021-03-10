cc234f911fc6d55aff5eb5c216be7c33743c21b4
Shorzinator <62899599+Shorzinator@users.noreply.github.com>
2021-03-10 13:16:10 +0530

    Importing specific modules

--- Breast Cancer Detection using KNN (Approach - 2)/SourceCode.py
+++ Breast Cancer Detection using KNN (Approach - 2)/SourceCode.py
@@ -13,3 +13,13 @@ print('matplotlib: {}'.format(matplotlib.__version__))
 print('pandas: {}'.format(pandas.__version__))
 print('sklearn: {}'.format(sklearn.__version__))
 
+import numpy as np
+from sklearn import preprocessing, cross_validation
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+import pandas as pd
